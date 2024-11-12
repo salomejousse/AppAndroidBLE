ea301912f0de60af9616442576160bc83afc36fb
Salome <salome.jousse@isen.yncrea.fr>
2024-11-12 17:01:33 +0100

    projet

--- app/src/main/java/fr/isen/jousse/androidsmartdevice/MainActivity.kt
+++ app/src/main/java/fr/isen/jousse/androidsmartdevice/MainActivity.kt
@@ -13,14 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
