f043438a6f64ac9d39c198f69688a7d5890b7784
Salome <salome.jousse@isen.yncrea.fr>
2024-11-20 18:01:32 +0100

    finalisation de l'app

--- app/src/main/java/fr/isen/jousse/androidsmartdevice/ConnexionActivity.kt
+++ app/src/main/java/fr/isen/jousse/androidsmartdevice/ConnexionActivity.kt
@@ -1,147 +1,235 @@
 package fr.isen.jousse.androidsmartdevice
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import fr.isen.jousse.androidsmartdevice.ui.theme.AndroidSmartDeviceTheme
+import androidx.core.app.ActivityCompat
+
+// Définition des états possibles des LEDs
+enum class LEDStateEnum(val value: Byte) {
+    OFF(0x00),
+    ON(0x01)
+}
 
 class ConnexionActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    private var bluetoothGatt: BluetoothGatt? = null
+    private var bluetoothDevice: BluetoothDevice? = null
+    private var ledCharacteristic: BluetoothGattCharacteristic? = null
+
+    // Remplace cette constante par l'UUID réel de la caractéristique LED
+    private val LED_UUID = "UUID_LED_CHARACTERISTIC"  // Exemple : "00002a56-0000-1000-8000-00805f9b34fb"
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val deviceAddress = intent.getStringExtra("DEVICE_ADDRESS") ?: "Adresse inconnue"
+        // Vérification de l'adresse Bluetooth avant de l'utiliser
+        bluetoothDevice = intent.getParcelableExtra<BluetoothDevice>("DEVICE")
 
         setContent {
-            AndroidSmartDeviceTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text("AndroidSmartDevice") },
-                            colors = TopAppBarDefaults.smallTopAppBarColors(
-                                containerColor = Color.Blue,
-                                titleContentColor = Color.White
-                            )
-                        )
-                    }
-                ) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = "Adresse :",
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier.padding(bottom = 4.dp)
-                        )
-                        Text(
-                            text = deviceAddress,
-                            fontSize = 16.sp,
-                            color = Color.Gray,
-                            modifier = Modifier.padding(bottom = 24.dp)
-                        )
+            DeviceScreen(deviceName = bluetoothDevice?.name ?: "", deviceAddress = bluetoothDevice?.address ?: "")
+        }
+    }
 
-                        Divider(color = Color.LightGray, thickness = 1.dp)
+    @SuppressLint("MissingPermission")
+    private fun connectToDevice() {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-                        Spacer(modifier = Modifier.height(16.dp))
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth non pris en charge", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-                        Image(
-                            painter = painterResource(id = R.drawable.led_img),
-                            contentDescription = "Led Image",
-                            modifier = Modifier.size(100.dp)
-                        )
 
-                        LedControlButton("Allumer LEDs") {
-                            Toast.makeText(this@ConnexionActivity, "Commande LEDs envoyée", Toast.LENGTH_SHORT).show()
-                        }
+        if (bluetoothDevice == null) {
+            Log.e("Bluetooth", "Aucun périphérique Bluetooth valide")
+            return
+        }
 
-                        Divider(color = Color.LightGray, thickness = 1.dp, modifier = Modifier.padding(vertical = 16.dp))
-
-                        ClickCounterButton(
-                            label = "Consulter le nombre de clics du bouton principal",
-                            onClick = {
-                                // Logique pour obtenir le nombre de clics du bouton principal
-                                Toast.makeText(this@ConnexionActivity, "Lecture des clics du bouton principal", Toast.LENGTH_SHORT).show()
-                            }
-                        )
-
-                        Divider(color = Color.LightGray, thickness = 1.dp, modifier = Modifier.padding(vertical = 16.dp))
-
-                        ClickCounterButton(
-                            label = "Consulter le nombre de clics du troisième bouton",
-                            onClick = {
-                                // Logique pour obtenir le nombre de clics du troisième bouton
-                                Toast.makeText(this@ConnexionActivity, "Lecture des clics du troisième bouton", Toast.LENGTH_SHORT).show()
-                            }
-                        )
-                    }
+        bluetoothGatt = bluetoothDevice?.connectGatt(this, false, object : BluetoothGattCallback() {
+            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+                Log.d("BLE", "Connexion État: $status, Nouvel état: $newState")
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.d("BLE", "Connecté au serveur GATT. Découverte des services...")
+                    gatt.discoverServices()
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.d("BLE", "Déconnecté du serveur GATT.")
+                    Toast.makeText(this@ConnexionActivity, "Déconnexion réussie", Toast.LENGTH_SHORT).show()
                 }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    val services = gatt.services
+                    for (service in services) {
+                        Log.d("BLE", "Service trouvé: ${service.uuid}")
+                        // Vérifie les caractéristiques de chaque service
+                        val characteristics = service.characteristics
+                        for (characteristic in characteristics) {
+                            Log.d("BLE", "Caractéristique trouvée: ${characteristic.uuid}")
+                            // On recherche la caractéristique LED ici
+                            if (characteristic.uuid.toString() == LED_UUID) {
+                                ledCharacteristic = characteristic
+                                break
+                            }
+                        }
+                    }
+                } else {
+                    Log.e("BLE", "Échec de la découverte des services avec le statut $status")
+                }
+            }
+
+            override fun onCharacteristicWrite(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    Log.d("BLE", "Caractéristique écrite avec succès: ${characteristic.uuid}")
+                } else {
+                    Log.e("BLE", "Échec de l'écriture sur la caractéristique: ${characteristic.uuid}")
+                }
+            }
+        })
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun writeToLEDCharacteristic(state: LEDStateEnum) {
+        if (ledCharacteristic != null) {
+            ledCharacteristic?.value = byteArrayOf(state.value) // Conversion en ByteArray
+            bluetoothGatt?.writeCharacteristic(ledCharacteristic)
+            Log.d("BLE", "LED state set to: ${state.name}")
+        } else {
+            Log.e("BLE", "LED characteristic non trouvée.")
+            Toast.makeText(this, "Caractéristique LED non trouvée", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun disconnectFromDevice() {
+        bluetoothGatt?.disconnect()
+        bluetoothGatt?.close()
+        bluetoothGatt = null
+        Log.d("BLE", "Déconnecté du périphérique.")
+        Toast.makeText(this, "Déconnecté du périphérique", Toast.LENGTH_SHORT).show()
+    }
+
+    @Composable
+    fun DeviceScreen(deviceName: String?, deviceAddress: String?) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Device Info", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Name: ${deviceName ?: "Inconnu"}")
+            Text("Address: ${deviceAddress ?: "Inconnu"}")
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Bouton pour se connecter au périphérique Bluetooth
+            Button(
+                onClick = {
+                    Log.d("Bluetooth", "Tentative de connexion...")
+                    connectToDevice()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
+                Text("Connecter au device", color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.led_img),
+                    contentDescription = "LED ON",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(
+                    onClick = { writeToLEDCharacteristic(LEDStateEnum.ON) },
+                    modifier = Modifier.padding(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) {
+                    Text("Allumer LED 1", color = Color.White)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Bouton pour éteindre la LED avec l'image de la LED
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_led_off),
+                    contentDescription = "LED OFF",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(
+                    onClick = { writeToLEDCharacteristic(LEDStateEnum.OFF) },
+                    modifier = Modifier.padding(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) {
+                    Text("Eteindre LED 1", color = Color.White)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Bouton pour se déconnecter du périphérique
+            Button(
+                onClick = { disconnectFromDevice() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
+                Text("Deconnecter le device", color = Color.White)
             }
         }
     }
-}
 
-@Composable
-fun LedControlButton(label: String, onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Blue
-        )
-    ) {
-        Text(text = label, color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
-    }
-}
-
-@Composable
-fun ClickCounterButton(label: String, onClick: () -> Unit) {
-    var clickCount by remember { mutableStateOf(0) }
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-    ) {
-        Button(
-            onClick = {
-                onClick()
-                clickCount += 1 // Incrémenter le compteur à chaque clic
-            },
-            modifier = Modifier.weight(1f),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
-        ) {
-            Text(text = label, color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        Box(
-            modifier = Modifier
-                .size(50.dp)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = clickCount.toString(), fontSize = 16.sp, fontWeight = FontWeight.Bold)
-        }
+    @SuppressLint("MissingPermission")
+    override fun onDestroy() {
+        super.onDestroy()
+        disconnectFromDevice()
     }
 }
--- app/src/main/java/fr/isen/jousse/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/jousse/androidsmartdevice/ScanActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.jousse.androidsmartdevice
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
@@ -16,18 +17,34 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,17 +64,26 @@ class ScanActivity : ComponentActivity() {
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
-        if (permissions.all { it.value }) {
+        val allPermissionsGranted = permissions.entries.all { it.value }
+        if (allPermissionsGranted) {
             Toast.makeText(this, "Permissions Bluetooth accordées", Toast.LENGTH_SHORT).show()
+            if (bluetoothAdapter.isEnabled) {
+                startScan()
+            } else {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, PERMISSION_REQUEST_CODE)
+            }
         } else {
             Toast.makeText(this, "Permissions Bluetooth requises", Toast.LENGTH_SHORT).show()
         }
     }
 
+
     private var isBluetoothEnabled by mutableStateOf(false)
     private var showBluetoothDialog by mutableStateOf(false)
     private var isScanning by mutableStateOf(false)
 
+    @SuppressLint("MissingPermission")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,10 +91,17 @@ class ScanActivity : ComponentActivity() {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
 
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
-            showBluetoothDialog = true
+        //if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
+            //showBluetoothDialog = true
+        //} else {
+            //isBluetoothEnabled = true
+        //}
+
+        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, PERMISSION_REQUEST_CODE)
         } else {
-            isBluetoothEnabled = true
+            isBluetoothEnabled = bluetoothAdapter.isEnabled == true
         }
 
         checkAndRequestPermissions()
@@ -128,8 +161,11 @@ class ScanActivity : ComponentActivity() {
         val permissionsToRequest = permissions.filter { permission ->
             ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED
         }
-        if (permissionsToRequest.isNotEmpty()) {
-            requestPermissionLauncher.launch(permissionsToRequest.toTypedArray())
+        if (permissions.isNotEmpty()) {
+            requestPermissionLauncher.launch(permissions.toTypedArray())
+        } else if(!bluetoothAdapter.isEnabled) {
+            val enableBIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBIntent, PERMISSION_REQUEST_CODE)
         } else {
             Toast.makeText(this, "Toutes les permissions sont déjà accordées", Toast.LENGTH_SHORT).show()
             startScan()
@@ -137,23 +173,70 @@ class ScanActivity : ComponentActivity() {
     }
 
     private fun getRequiredPermissions(): List<String> {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            listOf(
-                Manifest.permission.BLUETOOTH_SCAN,
-                Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.BLUETOOTH_ADMIN,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        } else {
-            listOf(
-                Manifest.permission.BLUETOOTH,
-                Manifest.permission.BLUETOOTH_ADMIN,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            )
+        return when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                listOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+                listOf(
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            }
+            else -> {
+                listOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
         }
     }
 
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 1
+    }
+
+    //override fun onRequestPermissionsResult(
+        //requestCode: Int,
+        //permissions: Array<out String>,
+        //grantResults: IntArray
+    //) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //if (requestCode == PERMISSION_REQUEST_CODE) {
+            //if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                //Toast.makeText(this, "Toutes les permissions sont accordées", Toast.LENGTH_SHORT).show()
+            //} else {
+                //Toast.makeText(this, "Permissions Bluetooth manquantes", Toast.LENGTH_SHORT).show()
+            //}
+        //}
+    //}
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                if (bluetoothAdapter.isEnabled) {
+                    startScan()
+                } else {
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    startActivityForResult(enableBtIntent, PERMISSION_REQUEST_CODE)
+                }
+            } else {
+                Toast.makeText(this, "Permissions Bluetooth manquantes", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
     private fun startScan() {
         bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
         devicesList.clear()
@@ -161,16 +244,22 @@ class ScanActivity : ComponentActivity() {
         Log.d("BluetoothScan", "Scan démarré")
     }
 
+    @SuppressLint("MissingPermission")
     private fun stopScan() {
         bluetoothLeScanner.stopScan(scanCallback)
         Log.d("BluetoothScan", "Scan arrêté")
     }
 
+    @SuppressLint("MissingPermission")
     private val scanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
+            // Vérifier si le scan est en cours
+            if (!isScanning) return
+
             val device = result.device
             val deviceAddress = device.address
             val deviceName = device.name ?: "Appareil inconnu"
+
             if (devicesList.none { it.device.address == result.device.address }) {
                 if (uniqueMacAddresses.contains(deviceAddress) || uniqueDeviceNames.contains(deviceName)) {
                     return
@@ -187,13 +276,25 @@ class ScanActivity : ComponentActivity() {
         }
     }
 
+
     @Composable
     fun StartStopScanButton(
         isScanning: Boolean,
         onClick: () -> Unit
     ) {
+        val context = LocalContext.current
         Button(
-            onClick = onClick,
+            onClick = {
+                val permissions = getRequiredPermissions()
+                val permissionGranted = permissions.all {
+                    ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+                }
+                if (permissionGranted){
+                    onClick()
+                } else {
+                    Toast.makeText(context, "Veuillez accorder les permissions Bluetooth", Toast.LENGTH_SHORT).show()
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
@@ -216,7 +317,7 @@ class ScanActivity : ComponentActivity() {
     fun DeviceList(devices: List<ScanResult>) {
         Column(modifier = Modifier.fillMaxWidth()) {
             devices.forEachIndexed { index, device ->
-                DeviceItem(deviceName = device.device.name ?: "Appareil inconnu", deviceNumber = "Adresse: ${device.device.address}")
+                DeviceItem(device = device.device)
                 if (index < devices.size - 1) {
                     Spacer(modifier = Modifier.height(8.dp))
                     Divider(
@@ -232,8 +333,9 @@ class ScanActivity : ComponentActivity() {
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Composable
-    fun DeviceItem(deviceName: String, deviceNumber: String) {
+    fun DeviceItem(device: BluetoothDevice) {
         val context = LocalContext.current
         Row(
             modifier = Modifier
@@ -243,21 +345,22 @@ class ScanActivity : ComponentActivity() {
                 .clickable {
                     // Création d'un Intent pour lancer l'activité DeviceInteractionActivity
                     val intent = Intent(context, ConnexionActivity::class.java).apply {
-                        putExtra("DEVICE_ADDRESS", deviceNumber) // Passer l'adresse de l'appareil
+                        putExtra("DEVICE", device)
                     }
+                    stopScan()
                     context.startActivity(intent)
                 },
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                text = deviceName,
+                text = device.name ?: "Inconnu",
                 color = Color.DarkGray,
                 fontWeight = FontWeight.Bold,
                 fontSize = 18.sp
             )
             Text(
-                text = deviceNumber,
+                text = device.address,
                 color = Color.Gray,
                 fontSize = 18.sp
             )
